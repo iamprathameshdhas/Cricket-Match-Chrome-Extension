176543a0c1e170d614cc62974df52acfec224627
Prathamesh Dhas <126050846+iamprathameshdhas@users.noreply.github.com>
2024-03-01 15:08:47 +0530

    Add files via upload

---
+++ Cricket Match Chrome Extension/script.js
@@ -0,0 +1,25 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=b07e2ff4-6797-48d1-8837-3d84a8b861e0&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
